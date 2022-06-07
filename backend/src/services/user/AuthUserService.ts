302b0cc0dc34561fe7a8e58f941149515a351b44
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-06-06 23:24:21 -0300

    Criptografando a senha

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,35 @@
+import prismaClient from '../../prisma';
+import { compare } from 'bcryptjs';
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    // Verificar se o email existe
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User/password incorrect');
+    }
+
+    // preciso verificar se a senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('User/password incorrect');
+    }
+
+    // gerar um token jwt e devolver os dados do usuário como id, name e email
+
+    return { ok: true };
+  }
+}
+
+export { AuthUserService };
