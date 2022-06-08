9e1323847d40006d5b15feed9bbdc5fe3302484e
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-06-07 23:47:26 -0300

    Cadastrando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
@@ -11,5 +12,8 @@ const router = Router();
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+// Rotas Category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
