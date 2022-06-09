636b0f81b68d1e43104d9a72a75c1f5bb1cb50fa
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-06-09 16:17:29 -0300

    Adicionando itens ao pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,7 @@ import { isAuthenticated } from './middlewares/isAuthenticated';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
 
 import uploadConfig from './config/multer';
 
@@ -34,5 +35,6 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // Rotas Order
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
 
 export { router };
