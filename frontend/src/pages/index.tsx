f2c9cdea2e47bc5a2b504ea8f4019814400543da
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-06-10 16:33:54 -0300

    Componente button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../../styles/home.module.scss';
 import logoImg from '../../public/logo.png';
 
 import { Input } from '../components/ui/Input/index';
+import { Button } from '../components/ui/Button/index';
 
 export default function Home() {
   return (
@@ -19,6 +20,9 @@ export default function Home() {
           <form action="">
             <Input placeholder="Digite seu email" type="text" />
             <Input placeholder="Sua senha" type="password" />
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
           </form>
         </div>
       </div>
