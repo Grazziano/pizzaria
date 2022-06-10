2feaa9b53948ce5723d64f3d0b082489a19a7424
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-06-10 13:16:23 -0300

    Criando document

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from '../../node_modules/next/head';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Best Pizza :)</h1>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Best Pizza - Faca seu login</title>
+      </Head>
+      <div>
+        <h1>Best Pizza</h1>
+      </div>
+    </>
+  );
 }
