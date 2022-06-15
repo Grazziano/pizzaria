8652ffa71d2b22e54f5b6d8946cd9c727cd52b5d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-15 20:47:12 -0300

    Página categoria

---
+++ frontend/src/pages/category/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { FormEvent, useState } from 'react';
+import { Header } from '../../components/Header';
+import styles from './styles.module.scss';
+
+export default function Category() {
+  const [name, setName] = useState('');
+
+  async function handleRegister(event: FormEvent) {
+    event.preventDefault();
+    alert('Categoria: ' + name);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Nova Categoria - Best Pizzaria</title>
+      </Head>
+      <div>
+        <Header />
+        <main className={styles.container}>
+          <h1>Cadastrar categorias</h1>
+          <form className={styles.form} onSubmit={handleRegister}>
+            <input
+              type="text"
+              placeholder="Digite o nome da categoria"
+              className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button className={styles.buttonAdd}>Cadastrar</button>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+}
