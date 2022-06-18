e33c7d01315f591d8ca267915e2e2523787fb6cd
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-18 20:42:50 -0300

    Tela login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
 export default function Routes() {
   const isAuthenticated = false;
   const loading = false;
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#1D1D2E',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ActivityIndicator size={60} color="#FFF" />
+      </View>
+    );
+  }
 
   return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 }
