79467c3042280c5fd7cae57ba7c6617025db193a
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-18 20:00:37 -0300

    Criando projeto

---
+++ mobile/App.tsx
@@ -0,0 +1,15 @@
+import { View, StatusBar } from 'react-native';
+import SignIn from './src/pages/SignIn';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar
+        backgroundColor="#1D1D2E"
+        barStyle="light-content"
+        translucent={false}
+      />
+      <SignIn />
+    </View>
+  );
+}
